c1a6a3c23709a504c74c3330bae0b0fcc8299251
Charlotte <charlotteschnx@gmail.com>
2024-12-05 02:30:50 +0100

    organized header file

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: cschnath <cschnath@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 01:30:31 by cschnath          #+#    #+#             */
-/*   Updated: 2024/12/02 14:04:31 by cschnath         ###   ########.fr       */
+/*   Updated: 2024/12/05 02:30:36 by cschnath         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,8 @@
 // Then we push that a node on top of its target node in b
 // If no closest smaller number is found, the target node is the max value in b
 // Cost analysis: Which a node is the cheapest to push to b
-// Push cost = (x operations to bring a on top) + (x operations to bring a->target node on top)
+// Push cost = (x operations to bring a on top)
+//	+ (x operations to bring a->target node on top)
 // If multiple nodes have the same cost, go with the first one
 // Then push every b node back to a
 // Every b node needs a target node in a which is the closest bigger number
@@ -43,19 +44,31 @@ typedef struct s_stack
 	int				push_cost;
 	bool			above_median;
 	bool			cheapest;
-    struct s_stack	*target_node;
+	struct s_stack	*target_node;
 	struct s_stack	*next;
-    struct s_stack	*prev;
+	struct s_stack	*prev;
 }					t_stack;
 
-void ft_free_stack(t_stack **stack);
-void ft_error(t_stack **a, t_stack **b, char **argv);
-bool ft_is_sorted(t_stack *a);
-bool ft_is_duplicate(t_stack *stack, long int nb);
-bool ft_is_number(char *str);
-void ft_init_a(t_stack **a, char **argv);
-int ft_stack_len(t_stack *stack);
-long ft_atol(const char *str);
-void ft_append_node(t_stack **stack, long int nb);
+// Checks
+bool				ft_is_number(char *str);
+bool				ft_is_duplicate(t_stack *stack, long int nb);
+bool				ft_is_sorted(t_stack *a);
+static int			ft_wordcount(char const *s, char c);
+static char			*ft_nextword(char *s, char c);
+
+// Node stuff
+void				ft_free_stack(t_stack **stack);
+void				ft_append_node(t_stack **stack, long int nb);
+void				ft_init_a(t_stack **a, char **argv);
+int					ft_stack_len(t_stack *stack);
+
+// Sorting
+void				ft_sort_three(t_stack **a);
+void				ft_sort_stacks(t_stack **a, t_stack **b);
+
+// Utils
+void				ft_error(t_stack **a, t_stack **b, char **argv);
+long				ft_atol(const char *str);
+t_stack				*find_min(t_stack *stack);
 
 #endif
